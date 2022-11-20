ca7f075689f81aa27d5153462205e49c04a58b2d
Pablo Martínez Vergara <pablomartiver7@gmail.com>
2022-11-20 15:09:06 +0100

    Create Facil_17.py

---
+++ Retos_Python/Faciles/Facil_17.py
@@ -0,0 +1,24 @@
+#Diferentes operaciones con tupla
+t:tuple = (2,4,3,5,4,6,7,8,6,1)
+
+#Encontrar los elementos de 3 a 5
+print (t[2:5])
+
+#Encontrar los 6 primeros elementos
+print (t[:6])
+
+#Muestra la tupla desde el 5 elemento hasta el final
+print (t[4:])
+
+#Muestra toda la tupla haciendo uso de [:]
+print (t[0:])
+
+#Muestra todos los elementos desde la posición 2 a la 9 de dos en dos
+print (t[1:9:2])
+
+#Devuelve la tupla con un salto cada 4 elementos
+print (t[::4])
+
+#Usa un step negativo para mostrar la tupla desde la posición 9 a la 2
+print (t[9:1:-1]) 
+
